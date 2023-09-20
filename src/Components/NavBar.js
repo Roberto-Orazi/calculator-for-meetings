bdb5a15a1f27b8f1c4d89368360fd2dda228fb09
Roberto-Orazi <robervgg@gmail.com>
2023-09-20 15:00:41 -0300

    prepare the json por build

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,26 +1,26 @@
 import { Nav } from "react-bootstrap"
-import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Inicio from "./Inicio"
 import Repartir from "./Repartir"
 import Contacto from "./Contacto"
 const NavBar = () => {
-    return(
-<Router>
- <Nav className="navfullsize">
-    <ul>
-        <li>
-        <Link to="/">Inicio</Link>
-        <Link to="/repartir">RepartiTe</Link>
-        <Link to="/contacto">Contacto</Link>
-        </li>
-    </ul>
- </Nav>
-  <Routes>
-                 <Route exact path='/' element={< Inicio />}></Route>
-                 <Route exact path='/repartir' element={< Repartir />}></Route>
-                 <Route exact path='/contacto' element={< Contacto />}></Route>
-          </Routes>
- </Router>   
+    return (
+        <Router>
+            <Nav className="navfullsize">
+                <ul>
+                    <li>
+                        <Link to="/">Inicio</Link>
+                        <Link to="/repartir">RepartiTe</Link>
+                        <Link to="/contacto">Contacto</Link>
+                    </li>
+                </ul>
+            </Nav>
+            <Routes>
+                <Route exact path='/' element={< Inicio />}></Route>
+                <Route exact path='/repartir' element={< Repartir />}></Route>
+                <Route exact path='/contacto' element={< Contacto />}></Route>
+            </Routes>
+        </Router>
     )
 }
 
